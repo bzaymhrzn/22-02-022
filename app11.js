745965a5187ce052b439b70baca3b6e95c39d4e0
bzaymhrzn <bzaymhrzn@gmail.com>
2022-02-24 21:12:37 -0500

    first commit

---
+++ app11.js
@@ -0,0 +1,111 @@
+//const myName = 'bzzzay';
+//const job = 'housekeeping Supervisor';
+//const birthYear = '1993';
+//const currentYear = '2022';
+
+
+//const aboutMe =
+//'I am ' +
+//myName +
+//' ' +
+//'a '+
+//job + 
+//' ' +
+//(currentYear - birthYear) +
+//' ' +
+//'and years old ' ;
+
+//console.log(aboutMe);
+
+
+
+//const aboutMe = `I am ${myName} a ${job} 
+//${currentYear - birthYear} and years old`;
+
+//console.log(aboutMe);
+
+
+
+//const year ='1991';
+
+///console.log('I am ' + String(17) + ' years old');
+
+
+
+////let n = '10' + 10;
+//n = n - 1;
+//console.log(n - 1)
+
+
+//console.log('100' > 15);
+
+//console.log('90' === 90);
+
+//if (false) {
+  //  console.log('I am True')
+ //} else {
+  //   console.log('I am false');
+ //}
+
+
+
+ //let isNumber;
+
+ //console.log(10 + 10 + '5' - 5);
+
+//const isNumber = 20;
+ 
+//if (isNumber) {
+//const myName = "bzzzay";
+//} else {
+// const myName = "Maharjan"}
+//const myName = isNumber ? 'bzzzay' : 'Maharjan';
+//console.log(myName);
+
+//console.log(`My name is ${isNumber > 20 ? 'bzzzay' + 'Maharjan'});
+
+// let day = "Tuesday"
+
+ //if (day=== 'Monday'){
+  //console.log('Monday');
+//} else {
+  //console.log('By!');
+//}
+
+//switch (day) {
+  //case "Sunday":
+  //console.log('Sunday');
+    //break;
+
+    //case 'Tuesday':
+    //console.log('Tuesday');
+    //break;
+
+    //default:
+     // console.log('No week days found');
+      //break;}
+
+//printMyAge();
+       //const printMyAge = function () {
+
+         
+        //console.log('Your age is 30');} 
+
+      //function printMyAge(){
+
+
+      //console.log(myName);
+      //var myName = 'Bzzzay';
+
+      //abc = 'aa';
+      //console.log(abc);
+      //var abc;
+
+    
+      
+
+
+
+        
+      
+
---
+++ app111.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Country
+const country = 'Nepal';
+const anotherCountry = 'USA';
+const countryArray = ['USA', 'NEPAL'];
+
+console.log(country, anotherCountry);
+console.log(countryArray[0]);
+
+const countryObject = {
+    countryTwo: 'USA',
+    countryOne: 'Nepal',
+};
+console.log(countryObject.countryOne);
